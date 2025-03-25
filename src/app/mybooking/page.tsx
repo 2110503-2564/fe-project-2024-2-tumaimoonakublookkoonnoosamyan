9edff3d74c899f37bed8e3aed1e9d5f827119386
Pattapon Surinwarangkun <pattaponoak@gmail.com>
2025-03-25 23:03:40 +0700

    minor change

--- src/app/mybooking/page.tsx
+++ src/app/mybooking/page.tsx
@@ -1,13 +1,5 @@
 import BookingList from "@/components/BookingList";
-import getUserAppointments from "@/libs/getUserAppointments";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/authOptions";
-
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-  console.log(session)
-  // const userApponements = getUserAppointments(session)
-
   return (
     <main>
         <BookingList />
